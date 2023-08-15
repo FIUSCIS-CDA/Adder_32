ccb3b920b7c35992bd57bbf48b3ce6cbcb527661
Trevor Cickovski <movingpictures83@gmail.com>
2023-08-15 13:03:08 -0400

    Added color, upgraded Quartus

--- Adder_32.v
+++ Adder_32.v
@@ -15,7 +15,7 @@
 
 // PROGRAM		"Quartus Prime"
 // VERSION		"Version 20.1.1 Build 720 11/11/2020 SJ Lite Edition"
-// CREATED		"Tue May 10 09:13:40 2022"
+// CREATED		"Tue Aug 15 13:01:40 2023"
 
 module Adder_32(
 	A,
